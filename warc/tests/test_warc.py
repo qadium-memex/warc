83a601af1e54891452846cf01f37cf7c43db9727
Sang Han <jjangsangy@gmail.com>
2016-07-01 13:38:23 -0700

    changed commit

--- warc/tests/test_warc.py
+++ warc/tests/test_warc.py
@@ -1,13 +1,16 @@
 from builtins import str
 from builtins import range
 from builtins import object
-from ..warc import WARCReader, WARCHeader, WARCRecord, WARCFile
 
-from io import StringIO
+from .. import warc
+
+import os.path
+
+from io import StringIO, BytesIO
 
 class TestWARCHeader(object):
     def test_attrs(self):
-        h = WARCHeader({
+        h = warc.WARCHeader({
             "WARC-Type": "response",
             "WARC-Record-ID": "<record-1>",
             "WARC-Date": "2000-01-02T03:04:05Z",
@@ -19,7 +22,7 @@ class TestWARCHeader(object):
         assert h.content_length == 10
 
     def test_item_access(self):
-        h = WARCHeader({
+        h = warc.WARCHeader({
             "WARC-Type": "response",
             "X-New-Header": "42"
         })
@@ -31,17 +34,17 @@ class TestWARCHeader(object):
         assert h['x-new-header'] == "42"
 
     def test_str(self):
-        h = WARCHeader({})
+        h = warc.WARCHeader({})
         assert str(h) == "WARC/1.0\r\n\r\n"
 
-        h = WARCHeader({
+        h = warc.WARCHeader({
             "WARC-Type": "response"
         })
         assert str(h) == "WARC/1.0\r\n" + "WARC-Type: response\r\n\r\n"
 
     def test_init_defaults(self):
         # It should initialize all the mandatory headers
-        h = WARCHeader({"WARC-Type": "resource"}, defaults=True)
+        h = warc.WARCHeader({"WARC-Type": "resource"}, defaults=True)
         assert h.type == "resource"
         assert "WARC-Date" in h
         assert "Content-Type" in h
@@ -49,7 +52,7 @@ class TestWARCHeader(object):
 
     def test_new_content_types(self):
         def f(type):
-            return WARCHeader({"WARC-Type": type}, defaults=True)
+            return warc.WARCHeader({"WARC-Type": type}, defaults=True)
         assert f("response")["Content-Type"] == "application/http; msgtype=response"
         assert f("request")["Content-Type"] == "application/http; msgtype=request"
         assert f("warcinfo")["Content-Type"] == "application/warc-fields"
@@ -71,41 +74,41 @@ SAMPLE_WARC_RECORD_TEXT = (
 class TestWARCReader(object):
     def test_read_header1(self):
         f = StringIO(SAMPLE_WARC_RECORD_TEXT)
-        h = WARCReader(f).read_record().header
+        h = warc.WARCReader(f).read_record().header
         assert h.date == "2012-02-10T16:15:52Z"
         assert h.record_id == "<urn:uuid:80fb9262-5402-11e1-8206-545200690126>"
         assert h.type == "response"
         assert h.content_length == 10
 
     def test_empty(self):
-        reader = WARCReader(StringIO(""))
+        reader = warc.WARCReader(StringIO(""))
         assert reader.read_record() is None
 
     def test_read_record(self):
         f = StringIO(SAMPLE_WARC_RECORD_TEXT)
-        reader = WARCReader(f)
+        reader = warc.WARCReader(f)
         record = reader.read_record()
         assert "".join(record.payload) == "Helloworld"
 
     def read_multiple_records(self):
         f = StringIO(SAMPLE_WARC_RECORD_TEXT * 5)
-        reader = WARCReader(f)
+        reader = warc.WARCReader(f)
         for i in range(5):
             rec = reader.read_record()
             assert rec is not None
 
 class TestWarcFile(object):
     def test_read(self):
-        f = WARCFile(fileobj=StringIO(SAMPLE_WARC_RECORD_TEXT))
+        f = warc.WARCFile(fileobj=StringIO(SAMPLE_WARC_RECORD_TEXT))
         assert f.read_record() is not None
         assert f.read_record() is None
 
     def test_write_gz(self):
         """Test writing multiple member gzip file."""
-        buffer = StringIO()
-        f = WARCFile(fileobj=buffer, mode="w", compress=True)
+        buffer = BytesIO()
+        f = warc.WARCFile(fileobj=buffer, mode="w", compress=True)
         for i in range(10):
-            record = WARCRecord(payload="hello %d" % i)
+            record = warc.WARCRecord(payload=b"hello %d" % i)
             f.write_record(record)
 
         GZIP_MAGIC_NUMBER = '\037\213'
@@ -113,11 +116,9 @@ class TestWarcFile(object):
 
     def test_long_header(self):
         """Test large WARC header with a CRLF across a 1024 byte boundrary"""
-        from .. import warc
-        import os.path
         warc_dir = os.path.dirname(warc.__file__)
         file = os.path.join(warc_dir, '../test_data/crlf_at_1k_boundary.warc.gz')
-        f = WARCFile(file)
+        f = warc.WARCFile(file)
         h = f.read_record().header
         assert h['WARC-Payload-Digest'] == "sha1:M4VJCCJQJKPACSSSBHURM572HSDQHO2P"
 
